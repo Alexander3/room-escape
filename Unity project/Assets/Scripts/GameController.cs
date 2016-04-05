b8b4f3bfe1cb8fc4b5054e45ef457f14b0ed88dc
Aleksander <kawalaaleksander@gmail.com>
2016-04-05 23:47:58 +0200

    First game logic: Poster -> Front Screens -> Swithbox

--- Unity project/Assets/Scripts/GameController.cs
+++ Unity project/Assets/Scripts/GameController.cs
@@ -1,17 +1,28 @@
-﻿using Scripts;
-using System;
-using System.Collections.Generic;
+﻿using UnityEngine;
 using System.Linq;
-using System.Text;
-using UnityEngine;
 
-namespace Assets
+
+class GameController : MonoBehaviour
 {
-    class GameController : MonoBehaviour
-    {
+	public delegate void Unlocker(string objectName);
+	public static event Unlocker Unlock;
+	public delegate void Locker(string objectName);
+	public static event Locker Lock;
 
-        public void Start()
-        {
-        }
+	private Color[] _screensColorsOrder	= { Color.green, Color.red, Color.blue };
+	private bool[] _screensColorsState = new bool[3];
+    public void Start()
+    {
+		ColorScreen.ScreenUsed += (short id, Color c) => {
+			if (c == _screensColorsOrder[id])
+				_screensColorsState[id] = true;
+			_screensColorsState.All(a => a);
+			if (_screensColorsState.All (x => x))
+				Unlock("SwitchBoxDoor");
+			else if(Lock != null)
+				Lock("SwitchBoxDoor");
+
+		};
+
     }
 }
--- Unity project/Assets/Scripts/UseableItems/Useable.cs
+++ Unity project/Assets/Scripts/UseableItems/Useable.cs
@@ -2,20 +2,31 @@
 using System.Collections;
 
 public abstract class Useable : MonoBehaviour {
-	public bool useOnlyOnce = false;
-	public float highlightSpeed = 6f;
-	public Color highlightColor = Color.white * 0.5f;
+	public bool UseOnlyOnce = false;
+	public float HighlightSpeed = 6f;
+	public Color AcceptColor = Color.white * 0.5f;
+	public static Color RejectColor = Color.red * 0.5f;
+	public bool CanBeUsed = true;
+
 	protected bool _alreadyUsed = false;   
 	protected bool _highlighting = false;
 	protected bool _highlight = true;
-	private Material _highlightedMaterial;
 	protected Material _material;
-	private Material _originalMaterial;
+	private Color _highlightColor;
 
 	protected void Start()
 	{
 		_material = GetComponent<Renderer>().material;
 		_material.EnableKeyword ("_EMISSION");
+		GameController.Unlock += (objectName) => {
+			if (gameObject.name == objectName)
+				CanBeUsed = true;
+		};
+		GameController.Unlock += (objectName) => {
+			if (gameObject.name == objectName)
+				CanBeUsed = false;
+		};
+				
 	}
 
 	protected void Update()
@@ -32,29 +43,30 @@ public abstract class Useable : MonoBehaviour {
 
 	public void HighlightItem()
 	{
-		if (useOnlyOnce && _alreadyUsed)
+		if (UseOnlyOnce && _alreadyUsed)
 			return;
 		_highlighting = true;
 		_highlight = true;
+		_highlightColor = CanBeUsed ? AcceptColor : RejectColor;
 	}
 
 	private void Highlight()
 	{
 		Color color = _material.GetColor ("_EmissionColor");
+
 		if (_highlight)
-			_material.SetColor("_EmissionColor", Color.Lerp(color, highlightColor, highlightSpeed * Time.deltaTime));
+			_material.SetColor("_EmissionColor", Color.Lerp(color, _highlightColor, HighlightSpeed * Time.deltaTime));
 		else
-			_material.SetColor("_EmissionColor", Color.Lerp(color, Color.black, highlightSpeed * Time.deltaTime));
-		if (color == Color.black || color == highlightColor)
+			_material.SetColor("_EmissionColor", Color.Lerp(color, Color.black, HighlightSpeed * Time.deltaTime));
+		if (color == Color.black || color == _highlightColor)
 			_highlighting = false;
 	}
 
 	public void Use(){
-		if (useOnlyOnce && _alreadyUsed)
+		if (UseOnlyOnce && _alreadyUsed || !CanBeUsed)
 			return;
 
 		doUse();
-
 		_alreadyUsed = true;
 	}
 
