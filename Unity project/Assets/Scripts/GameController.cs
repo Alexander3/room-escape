83b3c2f895089a0218b926a41bc4f23daf8a8336
Iwona <iwadar90@gmail.com>
2016-04-09 19:53:58 +0200

    hmm tv

--- Unity project/Assets/Scripts/GameController.cs
+++ Unity project/Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Linq;
 
 
@@ -6,6 +7,9 @@ class GameController : MonoBehaviour
 {
 	public GameObject Door;
 	public GameObject SwitchBoxDoor;
+    public GameObject Tvset4;
+    public GameObject SwitchBoxSlider2;
+    public GameObject pokretlo;
 
 	private Color[] _screensColorsOrder	= { Color.green, Color.red, Color.blue };
 	private bool[] _screensColorsState = new bool[3];
@@ -34,12 +38,30 @@ class GameController : MonoBehaviour
 
 		MoveSwitch.SwitchMoved += (short id, bool state) => {
 			_switchesState [id] = state;
-			if (_switchesState [0] == true) {
-				Door.GetComponent<Door>().enabled = true;
-				Debug.Log ("unlocked");
-			} else
-				Door.GetComponent<Door>().enabled = false;
-		};
+            if (_switchesState[0] == true)
+            {
+                //Door.GetComponent<Door>().enabled = true;
+                Tvset4.GetComponent<Useable>().CanBeUsed = true;
+                Debug.Log("unlocked");
+                 
+                   if (_switchesState[1] == true)
+                
+                      pokretlo.GetComponent<Useable>().CanBeUsed = true;
+                
+                   else
+                      pokretlo.GetComponent<Useable>().CanBeUsed = false;
+            }
+            else
+                //Door.GetComponent<Door>().enabled = false;
+                Tvset4.GetComponent<Useable>().CanBeUsed = false; ;
+        };
+
+        TextScreen.TvScreenUsed += () =>
+        {
+            SwitchBoxSlider2.GetComponent<Useable>().CanBeUsed = true;
+
+
+        };
 
     }
 }
---
+++ Unity project/Assets/Scripts/UseableItems/Actions/TextScreen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TextScreen : Useable
+{
+
+    public Text text;
+    public string code;
+
+
+    public delegate void TvScreenUse();
+    public static event TvScreenUse TvScreenUsed;
+
+    override protected void doUse()
+    {
+        text = GameObject.Find("Text").GetComponent<Text>();
+
+        text.text = code;
+        if (TvScreenUsed != null)
+            TvScreenUsed();
+
+    }
+}
